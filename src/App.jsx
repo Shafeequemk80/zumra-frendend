e98350ab946972f2910a9d7883e7d2e00f3c8a4d
Shafeequemk80 <shafeequemk80@gmail.com>
2024-07-20 09:18:27 +0530

    class note add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import TableReact from './Table';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import TableReact from "./Table";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const [name, setName] = useState('');
-  const [unit, setUnit] = useState('');
+  const [name, setName] = useState("");
+  const [unit, setUnit] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [data, setData] = useState([]);
@@ -18,22 +18,22 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const load = toast.loading('Loading..');
-        const response = await axios.get('https://zumra-backend.onrender.com/');
+        const load = toast.loading("Loading..");
+        const response = await axios.get("https://zumra-backend.onrender.com/");
         if (Array.isArray(response.data)) {
           toast.dismiss(load);
           setData(response.data);
           setCount(response.data.length);
         } else {
-          console.error('Received data is not an array:', response.data);
-          setError('Unexpected data format');
+          console.error("  Received data is not an array:", response.data);
+          setError("Unexpected data format");
         }
         setLoading(false);
       } catch (err) {
         setError(err.message);
         setLoading(false);
         toast.dismiss();
-        toast.error('Error loading data');
+        toast.error("Error loading data");
       }
     };
 
@@ -43,95 +43,124 @@ const App = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const load = toast.loading('Loading..');
-      const response = await axios.post('https://zumra-backend.onrender.com/', { name, unit });
-      console.log('Data sent successfully:', response.data);
+      const load = toast.loading("Loading..");
+      const response = await axios.post("https://zumra-backend.onrender.com/", {
+        name,
+        unit,
+      });
+      console.log("Data sent successfully:", response.data);
       setCount((prev) => prev + 1);
       toast.dismiss(load);
-      toast.success('Data added successfully!');
+      toast.success("Data added successfully!");
 
       setData((prev) => [response.data.data, ...prev]);
 
       const newTotalPages = Math.ceil((data.length + 1) / rowsLimit);
       setCurrentPage(newTotalPages - 1);
 
-      setName('');
-      setUnit('');
+      setName("");
+      setUnit("");
     } catch (error) {
-      console.error('Error sending data:', error);
+      console.error("Error sending data:", error);
       toast.dismiss();
-      toast.error('Error sending data');
+      toast.error("Error sending data");
     }
   };
 
   return (
     <div className="bg-gray-100">
-      <nav className="bg-light p-4">
-        <a className="flex items-center" href="#">
-          <img src="/ZUMRA.png" width="85" height="30" alt="ZUMRA Logo" />
-        </a>
-      </nav>
-
-      <div className="container mx-auto mt-6">
-        <h1 className="text-center text-3xl font-bold mb-4">ZUMRA</h1>
-        <h4 className="text-center text-xl mb-4">ഖുറതുൽ ഫുആദ്</h4>
-        <h1 className="text-center text-xl mb-4">
-          Zumra Count <span className="font-bold text-red-500">{count}</span>
-        </h1>
-
-        <div className="flex justify-center px-6">
-          <form id="nameForm" className="w-full max-w-md" onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label htmlFor="nameInput" className="block text-sm font-medium mb-1">Enter name:</label>
-              <input
-                type="text"
-                className="form-input w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                id="nameInput"
-                placeholder="Enter name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="myDropdown" className="block text-sm font-medium mb-1">Select unit:</label>
-              <select
-                id="myDropdown"
-                className="form-select w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-                value={unit}
-                onChange={(e) => setUnit(e.target.value)}
-                required
-              >
-                <option value="" disabled>Unit</option>
-                <option value="Velliparamba">Velliparamba</option>
-                <option value="5th mile">5th mile</option>
-                <option value="6th mile">6th mile</option>
-                <option value="Al fathah">Al fathah</option>
-                <option value="Kuttikkattoor">Kuttikkattoor</option>
-                <option value="Kuttipadam">Kuttipadam</option>
-                <option value="Kaniyath">Kaniyath</option>
-                <option value="Perya">Perya</option>
-                <option value="Anakuzhikkara">Anakuzhikkara</option>
-              </select>
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white py-2 px-4 rounded-md mb-10 shadow-sm hover:bg-blue-600"
-            >
-              Submit
-            </button>
-          </form>
-          <ToastContainer />
-        </div>
-        <TableReact
-          data={data}
-          loading={loading}
-          error={error}
-          currentPage={currentPage}
-          setCurrentPage={setCurrentPage}
-        />
+    <nav className="bg-light p-4">
+      <a className="flex items-center" href="#">
+        <img src="/ZUMRA.png" width="85" height="30" alt="ZUMRA Logo" />
+      </a>
+    </nav>
+  
+    <div className="container mx-auto mt-6 justify-center ">
+      <h1 className="text-center text-3xl font-bold mb-4">ZUMRA</h1>
+      <h4 className="text-center text-xl mb-4">ഖുറതുൽ ഫുആദ്</h4>
+      <h1 className="text-center text-xl mb-4">
+        Zumra Count <span className="font-bold text-red-500">{count}</span>
+      </h1>
+  
+      <div className="flex justify-center">
+        <button
+          onClick={() => window.open('https://drive.google.com/file/d/1w1HQEjm-GD8ckFpWMq_eUK8EuiXNEfXi', '_blank')}
+          className="text-center justify-center mb-4 bg-green-500 text-white px-5 shadow-sm hover:bg-green-600"
+        >
+          Class Note
+        </button>
       </div>
+    
+      <div className="flex justify-center px-6">
+        <form
+          id="nameForm"
+          className="w-full max-w-md"
+          onSubmit={handleSubmit}
+        >
+          <div className="mb-4">
+            <label
+              htmlFor="nameInput"
+              className="block text-sm font-medium mb-1"
+            >
+              Enter name:
+            </label>
+            <input
+              type="text"
+              className="form-input w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              id="nameInput"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="myDropdown"
+              className="block text-sm font-medium mb-1"
+            >
+              Select unit:
+            </label>
+            <select
+              id="myDropdown"
+              className="form-select w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={unit}
+              onChange={(e) => setUnit(e.target.value)}
+              required
+            >
+              <option value="" disabled>
+                Unit
+              </option>
+              <option value="Velliparamba">Velliparamba</option>
+              <option value="5th mile">5th mile</option>
+              <option value="6th mile">6th mile</option>
+              <option value="Al fathah">Al fathah</option>
+              <option value="Kuttikkattoor">Kuttikkattoor</option>
+              <option value="Kuttipadam">Kuttipadam</option>
+              <option value="Kaniyath">Kaniyath</option>
+              <option value="Perya">Perya</option>
+              <option value="Anakuzhikkara">Anakuzhikkara</option>
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded-md mb-10 shadow-sm hover:bg-blue-600"
+          >
+            Submit
+          </button>
+        </form>
+        <ToastContainer />
+      </div>
+      <TableReact
+        data={data}
+        loading={loading}
+        error={error}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
+  </div>
+  
   );
 };
 
